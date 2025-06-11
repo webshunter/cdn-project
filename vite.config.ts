179d56479c4168ffef5c7f4d559938dec77c6aa3
webshunter <webshunternet@gmail.com>
2025-06-11 11:07:30 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,11 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
-  server: {
-    headers: {
-      'Access-Control-Allow-Origin': 'https://appstore.gugusdarmayanto.my.id',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-      'Access-Control-Allow-Credentials': 'true'
-    }
-  },
   build: {
     rollupOptions: {
       output: {
         manualChunks: undefined
       }
-    }
-  },
-  preview: {
-    headers: {
-      'Access-Control-Allow-Origin': 'https://appstore.gugusdarmayanto.my.id',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-      'Access-Control-Allow-Credentials': 'true'
     }
   }
 });
